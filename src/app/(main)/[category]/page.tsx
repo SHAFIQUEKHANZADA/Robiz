9f60211a1ba6893e2f17afb974981eabea81c2ea
Shafique Ur Rehman <kzshafique77@gmail.com>
2025-01-02 12:54:46 +0500

    ui done

--- src/app/(main)/[category]/page.tsx
+++ src/app/(main)/[category]/page.tsx
@@ -7,6 +7,16 @@ import Image from "next/image";
 import { urlFor } from "@/sanity/lib/image";
 import Link from "next/link";
 import FilterAndSort from "@/app/components/Filter";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 const archivo = Archivo({ subsets: ["latin"], weight: ["400"] });
 
@@ -73,7 +83,9 @@ const CategoryPage = ({ params }: { params: { category: string } }) => {
   };
 
   if (loading) {
-    return <p>Loading...</p>;
+    return <div className="h-screen w-full flex justify-center items-center skeleton-loader">
+    <Image src={"/images/logo.png"} alt="logo" width={150} height={150} className="md:w-[250px] w-[100px] pt-10" />
+  </div>;
   }
 
   if (error) {
@@ -92,7 +104,7 @@ const CategoryPage = ({ params }: { params: { category: string } }) => {
   };
 
   return (
-    <div className={`${archivo.className} mb-10`}>
+    <div className={`${archivo.className} mb-7 mt-3`}>
       <p
         data-aos="fade-up"
         data-aos-duration="600"
@@ -103,46 +115,112 @@ const CategoryPage = ({ params }: { params: { category: string } }) => {
 
 
       {/* Filter div */}
-      <div className="flex items-center justify-between gap-2 sm:px-10 px-4 sticky top-0 sm:py-10 py-4 stickys ">
-        <div className="text-sm">
-          {/* Show results count */}
-          <p className="text-[#7C7C7C]">{sortedProducts.length} Results</p>
-        </div>
+      <div className="flex items-center justify-between gap-2 sm:px-10 px-4 sticky top-0 sm:py-9 py-4 stickys">
+        <div className="flex items-center gap-3">
+          <div className="text-sm">
+            {/* Show results count */}
+            <p className="text-[#7C7C7C]">{sortedProducts.length} Results</p>
+          </div>
+          <span className="bg-[#DFDFDF] w-[1px] h-5 mx-1"></span>
+          <div className="flex items-center gap-5">
+            <div className="flex items-center gap-2 hover:border hover:border-black p-2 border-[#F7F7F7] border">
+              <input type="checkbox" className="sm:w-5 sm:h-5 h-4 w-4 rounded-none bg-[#F7F7F7] border border-gray-200 checked:bg-transparent" />
+              <h1 className="text-[14px]">{params.category} <span className="text-[#7C7C7C]">({sortedProducts.length})</span></h1>
+            </div>
 
-      <div className="sm:flex hidden items-center gap-2">
-          {/* Filter dropdown */}
-          <div className="flex justify-center">
-          <select
-            onChange={(e) => handleSort(e.target.value as "highToLow" | "lowToHigh")}
-            className="bg-transparent"
-          >
-            <option value="" selected>Sort:</option>
-            <option value="lowToHigh">Price: Low to High</option>
-            <option value="highToLow">Price: High to Low</option>
-          </select>
-        </div>
-
-        {/* Filter dots */}
-        <div className="items-center gap-2 hidden sm:flex">
-          <h1 className="text-[12px] text-[#7C7C7C]">View</h1>
-          <div className="justify-center flex items-center">
-            <button
-              onClick={() => handleColumnChange(3, "md")}
-              className={`w-[10px] h-[10px] mx-[1px] ${columns === 3 ? "bg-black" : "bg-transparent border border-black"}`}
-            ></button>
-            <button
-              onClick={() => handleColumnChange(4, "lg")}
-              className={`w-[10px] h-[10px] mx-[1px] ${columns === 4 ? "bg-black" : "bg-transparent border border-black"}`}
-            ></button>
-            <button
-              onClick={() => handleColumnChange(5, "xl")}
-              className={`w-[10px] h-[10px] mx-[1px] ${columns === 5 ? "bg-black" : "bg-transparent border border-black"}`}
-            ></button>
+            <div className="border hover:border-black px-1 border-[#f7f7f7] sm:block hidden">
+              <Select>
+                <SelectTrigger className="w-full space-x-2 p-0  bg-transparent rounded-none border-none focus:outline-none">
+                  <SelectValue placeholder="Availability" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Fruits</SelectLabel>
+                    <SelectItem value="apple">Apple</SelectItem>
+                    <SelectItem value="banana">Banana</SelectItem>
+                    <SelectItem value="blueberry">Blueberry</SelectItem>
+                    <SelectItem value="grapes">Grapes</SelectItem>
+                    <SelectItem value="pineapple">Pineapple</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="border-[#f7f7f7] border hover:border-black px-1 sm:block hidden">
+              <Select>
+                <SelectTrigger className="w-full p-0 space-x-2  bg-transparent rounded-none border-none focus:outline-none">
+                  <SelectValue placeholder="Size" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Fruits</SelectLabel>
+                    <SelectItem value="apple">Apple</SelectItem>
+                    <SelectItem value="banana">Banana</SelectItem>
+                    <SelectItem value="blueberry">Blueberry</SelectItem>
+                    <SelectItem value="grapes">Grapes</SelectItem>
+                    <SelectItem value="pineapple">Pineapple</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="border-[#f7f7f7] border hover:border-black px-1 sm:block hidden">
+              <Select>
+                <SelectTrigger className="w-full space-x-2 p-0 bg-transparent rounded-none border-none focus:outline-none">
+                  <SelectValue placeholder="Sub-Category" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Fruits</SelectLabel>
+                    <SelectItem value="apple">Apple</SelectItem>
+                    <SelectItem value="banana">Banana</SelectItem>
+                    <SelectItem value="blueberry">Blueberry</SelectItem>
+                    <SelectItem value="grapes">Grapes</SelectItem>
+                    <SelectItem value="pineapple">Pineapple</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
           </div>
         </div>
-      </div>
 
-      <FilterAndSort/>
+        <div className="sm:flex hidden items-center gap-2">
+          {/* Filter dropdown */}
+          <div className="flex justify-center">
+            <Select onValueChange={(value) => handleSort(value as "highToLow" | "lowToHigh")}>
+              <SelectTrigger className="w-full p-0 bg-transparent border-none space-x-2 rounded-md focus:outline-none">
+                <SelectValue placeholder="Sort: Price" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Sort Options</SelectLabel>
+                  <SelectItem value="lowToHigh">Price: Low to High</SelectItem>
+                  <SelectItem value="highToLow">Price: High to Low</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+          <span className="bg-[#DFDFDF] w-[1px] h-5 mx-1"></span>
+
+          {/* Filter dots */}
+          <div className="items-center gap-2 hidden sm:flex">
+            <h1 className="text-[12px] text-[#7C7C7C]">View</h1>
+            <div className="justify-center flex items-center">
+              <button
+                onClick={() => handleColumnChange(3, "md")}
+                className={`w-[10px] h-[10px] mx-[1px] ${columns === 3 ? "bg-black" : "bg-transparent border border-black"}`}
+              ></button>
+              <button
+                onClick={() => handleColumnChange(4, "lg")}
+                className={`w-[10px] h-[10px] mx-[1px] ${columns === 4 ? "bg-black" : "bg-transparent border border-black"}`}
+              ></button>
+              <button
+                onClick={() => handleColumnChange(5, "xl")}
+                className={`w-[10px] h-[10px] mx-[1px] ${columns === 5 ? "bg-black" : "bg-transparent border border-black"}`}
+              ></button>
+            </div>
+          </div>
+        </div>
+
+        <FilterAndSort />
       </div>
 
       {/* Grid display */}
@@ -161,7 +239,7 @@ const CategoryPage = ({ params }: { params: { category: string } }) => {
                 height={500}
                 className="w-full h-[85%] object-cover object-top"
               />
-              <div className="text-center mt-2">
+              <div className="text-center my-1 p-2">
                 <h3 className="line-clamp-1">{product.title}</h3>
                 {product.salePrice ? (
                   <div className="flex justify-center items-center gap-3 text-[16px] sm:mt-2">
--- src/app/components/Filter.tsx
+++ src/app/components/Filter.tsx
@@ -8,6 +8,12 @@ import {
 } from "@/components/ui/sheet";
 import { ArrowDownUp } from "lucide-react";
 import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 // Filter Component
 export const Filter = () => {
@@ -15,17 +21,103 @@ export const Filter = () => {
     <Sheet>
       <SheetTrigger asChild>
         <div className="p-1 rounded-full border border-black cursor-pointer">
-          <Image src={"/svg/filter.svg"} alt="filter" width={100} height={100} className="h-5 w-5"/>
+          <Image
+            src={"/svg/filter.svg"}
+            alt="filter"
+            width={100}
+            height={100}
+            className="h-5 w-5"
+          />
         </div>
       </SheetTrigger>
-      <SheetContent className="bg-[#F7F7F7]">
+      <SheetContent className="bg-[#F7F7F7] p-4 sm:w-[400px]">
         <SheetHeader>
-          <SheetTitle className="font-light text-[18px] text-left">Filter</SheetTitle>
-          <div className="w-full h-[1.5px] bg-black mt-10"></div>
+          <SheetTitle className="font-light text-[18px] text-left">
+            Filter
+          </SheetTitle>
+          <div className="w-full h-[1.5px] bg-black mt-4"></div>
         </SheetHeader>
-        {/* Add filter options or content here */}
-        <div className="mt-4">
-          <p>Filter options will go here.</p>
+        <div className="mt-4 space-y-4">
+
+          <Accordion type="multiple" className="text-[14px]">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Availability</AccordionTrigger>
+              <AccordionContent>
+                <div className="space-y-2">
+                  <label className="flex items-center space-x-2">
+                    <input
+                      type="radio"
+                      name="availability"
+                      value="in-stock"
+                      className="w-5 h-5 bg-transparent rounded-full border-gray-300 focus:ring-2 focus:ring-blue-500"
+                    />
+                    <span>In stock</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input
+                      type="radio"
+                      name="availability"
+                      value="out-of-stock"
+                      className="w-5 h-5 bg-transparent rounded-full border-gray-300 focus:ring-2 focus:ring-blue-500"
+                    />
+                    <span>Out of stock</span>
+                  </label>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="item-2">
+              <AccordionTrigger>Size</AccordionTrigger>
+              <AccordionContent>
+                <div className="space-y-4">
+                  <label className="flex items-center space-x-2">
+                    <input type="checkbox" className="w-5 h-5 bg-transparent rounded border-gray-300 focus:ring-2 focus:ring-blue-500" />
+                    <span>XS</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input type="checkbox" className="w-5 h-5 bg-transparent rounded border-gray-300 focus:ring-2 focus:ring-blue-500" />
+                    <span>S</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input type="checkbox" className="w-5 h-5 bg-transparent rounded border-gray-300 focus:ring-2 focus:ring-blue-500" />
+                    <span>M</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input type="checkbox" className="w-5 h-5 bg-transparent rounded border-gray-300 focus:ring-2 focus:ring-blue-500" />
+                    <span>L</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input type="checkbox" className="w-5 h-5 bg-transparent rounded border-gray-300 focus:ring-2 focus:ring-blue-500" />
+                    <span>XL</span>
+                  </label>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+
+            <AccordionItem value="item-3">
+              <AccordionTrigger>Sub Category</AccordionTrigger>
+              <AccordionContent>
+                <div className="space-y-4">
+                  <label className="flex items-center space-x-2">
+                    <input type="checkbox" className="w-5 h-5 bg-transparent rounded border-gray-300 focus:ring-2 focus:ring-blue-500" />
+                    <span>Shirt</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input type="checkbox" className="w-5 h-5 bg-transparent rounded border-gray-300 focus:ring-2 focus:ring-blue-500" />
+                    <span>Pant</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input type="checkbox" className="w-5 h-5 bg-transparent rounded border-gray-300 focus:ring-2 focus:ring-blue-500" />
+                    <span>Dress</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input type="checkbox" className="w-5 h-5 bg-transparent rounded border-gray-300 focus:ring-2 focus:ring-blue-500" />
+                    <span>Shalwar kamez</span>
+                  </label>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
       </SheetContent>
     </Sheet>
@@ -38,7 +130,7 @@ export const Sort = () => {
     <Sheet>
       <SheetTrigger asChild>
         <div className="p-1 rounded-full border border-black cursor-pointer">
-          <ArrowDownUp className="h-5 w-5"/>
+          <ArrowDownUp className="h-5 w-5" />
         </div>
       </SheetTrigger>
       <SheetContent className="bg-[#F7F7F7]">
@@ -48,7 +140,26 @@ export const Sort = () => {
         </SheetHeader>
         {/* Add sort options or content here */}
         <div className="mt-4">
-          <p>Sort options will go here.</p>
+          <div className="space-y-2">
+            <label className="flex items-center space-x-2">
+              <input
+                type="radio"
+                name="availability"
+                value="in-stock"
+                className="w-5 h-5 bg-transparent rounded-full border-gray-300 focus:ring-2 focus:ring-blue-500"
+              />
+              <span>In stock</span>
+            </label>
+            <label className="flex items-center space-x-2">
+              <input
+                type="radio"
+                name="availability"
+                value="out-of-stock"
+                className="w-5 h-5 bg-transparent rounded-full border-gray-300 focus:ring-2 focus:ring-blue-500"
+              />
+              <span>Out of stock</span>
+            </label>
+          </div>
         </div>
       </SheetContent>
     </Sheet>
